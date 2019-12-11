a18f58297d698af8d46cd0b8f63bcc1c55d7a80b
Fatima Gulomova <fatimagulomova@gmail.com>
2019-12-11 14:04:53 +0200

    added spaces to match pep8

--- homework_1/task_1.py
+++ homework_1/task_1.py
@@ -4,17 +4,17 @@
 from builtins import len, repr
 
 lst = [10, 20, 30, 40, 50, 60, 70]
-n=0
+n = 0
 for i in lst:
-    n+=i
+    n += i
 print(n)
 
 #sum while
-n=0
-i=0
+n = 0
+i = 0
 while i < len(lst):
-    n+=lst[i]
-    i+=1
+    n += lst[i]
+    i += 1
 print(n)
 
 #sum recursion
