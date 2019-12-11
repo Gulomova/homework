dbe1902a1584d343e8f47e5cdab8329a8552ce79
Fatima Gulomova <fatimagulomova@gmail.com>
2019-12-11 17:34:15 +0200

    a program that takes a sequence of words separated by spaces as input data and prints the words after deleting all repeating words and sorting them in alphanumeric order

---
+++ homework_1/question_10.py
@@ -0,0 +1,10 @@
+# Question 10
+
+word = input().split()
+
+for i in word:
+    if word.count(i) > 1:    #count function returns total repeatation of an element that is send as argument
+        word.remove(i)     # removes exactly one element per call
+
+word.sort()
+print(" ".join(word))
