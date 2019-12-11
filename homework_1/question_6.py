1c2f85d771fbc020f8768738401b359a8fba92b5
Fatima Gulomova <fatimagulomova@gmail.com>
2019-12-11 11:58:42 +0200

    add question_6.py file to question_6 branch

---
+++ homework_1/question_6.py
@@ -0,0 +1,16 @@
+# Question 6
+from builtins import input, round, int, str
+from math import *
+
+C,H = 50,30
+
+def calc(D):
+    return sqrt((2*C*D)/H)
+
+D = input().split(',')
+D = [int(i) for i in D]
+D = [calc(i) for i in D]
+D = [round(i) for i in D]
+D = [str(i) for i in D]
+
+print(",".join(D))
