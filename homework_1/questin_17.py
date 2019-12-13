1f1120a7c5f14c35b65f990e6a93d729271a3a36
Fatima Gulomova <fatimagulomova@gmail.com>
2019-12-13 14:43:16 +0200

    a program that calculates the net amount of a bank account based on the transaction log from the console

---
+++ homework_1/questin_17.py
@@ -0,0 +1,15 @@
+#  Question 17
+
+total = 0
+while True:
+    s = input().split()
+    if not s:
+        break
+    cm, num = map(str, s)
+
+    if cm == 'D':
+        total += int(num)
+    if cm == 'W':
+        total -= int(num)
+
+print(total)
