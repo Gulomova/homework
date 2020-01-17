374ee0f0e8de1deac53782adc6b9e5730a53f45c
Fatima Gulomova <fatimagulomova@gmail.com>
2020-01-17 15:25:59 +0200

    code refactiring

--- homework_1/question_6.py
+++ homework_1/question_6.py
@@ -2,10 +2,12 @@
 from builtins import input, round, int, str
 from math import *
 
-C,H = 50,30
+C, H = 50, 30
+
 
 def calc(D):
     return sqrt((2*C*D)/H)
+
 
 D = input().split(',')
 D = [int(i) for i in D]
