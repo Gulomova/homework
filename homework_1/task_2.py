34424bc882975c9d78c0d3b95001e6c6a7d69f9a
Fatima Gulomova <fatimagulomova@gmail.com>
2020-01-22 16:44:53 +0200

    code review

--- homework_1/task_2.py
+++ homework_1/task_2.py
@@ -1,22 +1,12 @@
 # TASK №2
 
-
-def combine_lists(a, b):
-    len_a = len(a)
-    len_b = len(b)
-    if len_a < len_b:
-        limit = len_a
-    else:
-        limit = len_b
-    i = 0
-    r = []
-    while i < limit:
-        r.append(a[i])
-        r.append(b[i])
-        i += 1
-    return r
-
-if __name__ == '__main__':
-    a = [1, 2, 3]
-    b = [11, 22, 33]
-    print(repr(combine_lists(a, b)))
+a = [1, 2, 5, 4]
+b = [11, 22, 33]
+c = a + b
+c = [str(item) for item in c]
+# f = []
+# for item in c:
+#     f.append(str(item))
+c.sort()
+c = [int(item) for item in c]
+print(c)
